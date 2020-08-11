56466a75656ec92618cddaa9cf5088d7d9fd4ed5
Harsh Lakhanpal <harshlakhanpaln@gmail.com>
2020-08-11 23:24:08 +0530

    Loader fixes ,  Register mutation with loading

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { graphql } from "react-apollo";
 import { useLazyQuery, useMutation } from "@apollo/react-hooks";
 // import * as compose from "lodash.flowright";
+import Loader from "./Loader";
 import { register } from "../queries";
 import { useHistory } from "react-router-dom";
 
@@ -9,7 +10,7 @@ const Login = () => {
   const history = useHistory();
   const [values, setValues] = useState({});
 
-  const [userRegister] = useMutation(register);
+  const [userRegister, { loading }] = useMutation(register);
 
   const handleChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
@@ -27,52 +28,55 @@ const Login = () => {
   };
 
   return (
-    <div className="register neumorphic">
-      <input
-        className="input"
-        name="username"
-        value={values.username}
-        onChange={handleChange}
-        placeholder="Username"
-      />
-      <input
-        className="input"
-        name="email"
-        value={values.email}
-        onChange={handleChange}
-        placeholder="Email"
-      />
-      <input
-        type="password"
-        className="input"
-        name="password"
-        value={values.password}
-        //   onPressEnter={handleLogin}
-        onChange={handleChange}
-        placeholder="Password"
-      />
-      <input
-        type="password"
-        className="input"
-        name="confirmPassword"
-        value={values.confirmPassword}
-        //   onPressEnter={handleLogin}
-        onChange={handleChange}
-        placeholder="Confirm Password"
-      />
-      <br />
+    <>
+      {loading && <Loader />}
+      <div className="register neumorphic">
+        <input
+          className="input"
+          name="username"
+          value={values.username}
+          onChange={handleChange}
+          placeholder="Username"
+        />
+        <input
+          className="input"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          className="input"
+          name="password"
+          value={values.password}
+          //   onPressEnter={handleLogin}
+          onChange={handleChange}
+          placeholder="Password"
+        />
+        <input
+          type="password"
+          className="input"
+          name="confirmPassword"
+          value={values.confirmPassword}
+          //   onPressEnter={handleLogin}
+          onChange={handleChange}
+          placeholder="Confirm Password"
+        />
+        <br />
 
-      <button
-        className="input"
-        type="primary"
-        onClick={userRegistration}
-        //   loading={loading}
-        //   disabled={!username.length || !password.length}
-      >
-        Register
-      </button>
-      <br />
-    </div>
+        <button
+          className="input"
+          type="primary"
+          onClick={userRegistration}
+          //   loading={loading}
+          //   disabled={!username.length || !password.length}
+        >
+          Register
+        </button>
+        <br />
+      </div>
+    </>
   );
 };
 
