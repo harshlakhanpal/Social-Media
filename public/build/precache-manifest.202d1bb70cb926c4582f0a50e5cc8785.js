c9405790f3a5d81723b309c719ca356678304a88
Harsh Lakhanpal <harshlakhanpaln@gmail.com>
2020-08-20 17:48:59 +0530

    Deployment build with config vars for heroku with error handling

---
+++ public/build/precache-manifest.202d1bb70cb926c4582f0a50e5cc8785.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa6300a2dc7833f0a9d2f8f56c803aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15029fced5a1f7b486f7",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "814da05082004eb43e1f",
+    "url": "/static/css/main.4adc7059.chunk.css"
+  },
+  {
+    "revision": "15029fced5a1f7b486f7",
+    "url": "/static/js/2.423e59fe.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.423e59fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814da05082004eb43e1f",
+    "url": "/static/js/main.7885f985.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "d28f9fced090537958716faa7ab2c487",
+    "url": "/static/media/Add.d28f9fce.svg"
+  },
+  {
+    "revision": "68ee63aa77dbc2ef1507419c33dc9f99",
+    "url": "/static/media/delete.68ee63aa.svg"
+  },
+  {
+    "revision": "3c2916f30786ad091a3fc8ae95b6aaf2",
+    "url": "/static/media/heart-filled.3c2916f3.svg"
+  },
+  {
+    "revision": "0a029a2002403b6a6419bf696737888e",
+    "url": "/static/media/heart.0a029a20.svg"
+  },
+  {
+    "revision": "8ab7e7675a9118d74d5151ad7fa58840",
+    "url": "/static/media/loader.8ab7e767.gif"
+  },
+  {
+    "revision": "e797dbae16edc55498b83e54e63a3761",
+    "url": "/static/media/logout.e797dbae.svg"
+  },
+  {
+    "revision": "5d1abd5e381478a795fedacd8e5bca2a",
+    "url": "/static/media/right-arrow.5d1abd5e.svg"
+  },
+  {
+    "revision": "eccd1885d0f037ef62662ed47218a280",
+    "url": "/static/media/view-arrow.eccd1885.svg"
+  }
+]);
